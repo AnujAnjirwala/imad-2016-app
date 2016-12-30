216824992c30c27be3b0420366eef91c5cd96c07
AnujAnjirwala <15bit001@nirmauni.ac.in>
2016-12-30 17:20:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res){
-   res.send("Hi, this is articcle one."); 
+   res.send('Hi, this is articcle one.'); 
 });
 
 app.get('/article-two', function (req, res){
-   res.send("Hi, this is article two."); 
+   res.send('Hi, this is article two.'); 
 });
 
 app.get('/article-three', function (req, res){
-   res.send("Hi, this is article three."); 
+   res.send('Hi, this is article three.'); 
 });
 
 app.get('/ui/style.css', function (req, res) {
