34f2658a0b63db05b9129c53b0517705d36ab4a9
AnujAnjirwala <15bit001@nirmauni.ac.in>
2017-01-02 22:01:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ console.log('Loaded!');
 //change the text in index.html by coding in client-side JS.
 var ele=document.getElementById('maintext');
 ele.innerHTML='This is new Text!';
+
+//move img
+var img=document.getElementById('madi');
+img.onclick = function()    {
+    img.style.marginLeft = '100px';
+};
