82b74c331cf4d09c5f66b66262b49bd010af7870
AnujAnjirwala <15bit001@nirmauni.ac.in>
2017-01-02 21:53:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,4 +2,4 @@ console.log('Loaded!');
 
 //change the text in index.html by coding in client-side JS.
 var ele=document.getElementById('maintext');
-ele.innerHtml='This is new Text!'
+ele.innerHtml='This is new Text!';
