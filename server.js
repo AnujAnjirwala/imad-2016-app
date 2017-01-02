9454f8be01257b787db41efc80c2afd04b064805
AnujAnjirwala <15bit001@nirmauni.ac.in>
2017-01-02 18:44:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,6 +14,41 @@ var articleOne = {
             <p>This is some personal information.This is some personal information.This is some personal information.</p>`
 };
 
+function createTemplate(data)   {
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+var htmlTemlate = 
+   `<html>
+    <head>
+        <title> 
+            ${title} 
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+        <div>
+            <a href="/">Home</a>
+        </div>
+        <div>
+            <h1>${heading}</h1>
+        </div>
+        <div>
+            ${date}
+        </div>
+        <div>
+            ${content}
+        </div>
+        </div>
+    </body>
+</html>
+`;
+return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
