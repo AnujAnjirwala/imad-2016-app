464153ae23809a19f23d31af11c577e6c908e30a
AnujAnjirwala <15bit001@nirmauni.ac.in>
2017-01-03 20:40:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,11 +19,21 @@ img.onclick = function()    {
    var interval = setInterval(moveRight, 50);
 };
 
-var counter = 0;
+//var counter = 0;
 var c=document.getElementById('counter');
-
 c.onclick = function()  {
-    counter = counter + 1;
-    var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
+//    counter = counter + 1;
+    //create a request
+      var request = new XMLHttpRequest();
+      
+      //capture the response and store it in a variable.
+      request.onreadystatechange = function()   {
+        if(request.readystate === XMLHttpRequest.DONE)    {
+            if(request.status === 200)  {
+                var counter = request.responseText;
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+        }  
+      };
 };
