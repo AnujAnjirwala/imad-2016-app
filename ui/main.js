01b7fb2538fe919faaba78cc168883676ee91755
AnujAnjirwala <15bit001@nirmauni.ac.in>
2017-01-04 22:50:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -52,6 +52,28 @@ c.onclick = function()  {
       request.send(null);
 };
 
+var c=document.getElementById('counter');
+c.onclick = function()  {
+    //create a request
+      var request = new XMLHttpRequest();
+      
+      //capture the response and store it in a variable.
+      request.onreadystatechange = function()   {
+        if(request.readystate === XMLHttpRequest.DONE)    {
+            if(request.status === 200)  {
+                for(var i=0; i< names.length; i++)    {
+                    list += '<li>' + names[i] + '</li>';
+                 }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML=list;
+            }
+        }  
+      };
+      //Make the request
+      request.open('GET', 'http://anujanjirwala.imad.hasura-app.io/submit-name?name='+name, true);
+      request.send(null);
+};
+
 //submit name
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
@@ -61,9 +83,5 @@ submit.onclick = function() {
   //Capture a list of name and render it as list.
   var names = ['name1', 'name2', 'name3', 'name4' ];
   var list = '';
-  for(var i=0; i< names.length; i++)    {
-      list += '<li>' + names[i] + '</li>';
-  }
-  var ul = document.getElementById('namelist');
-  ul.innerHTML=list;
+  
 };
