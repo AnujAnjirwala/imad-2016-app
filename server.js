3cdce2a76bd296914c8cad1899d1f593179ec432
AnujAnjirwala <15bit001@nirmauni.ac.in>
2017-01-04 22:42:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,9 +91,11 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 var names=[];
-app.get('/submit-name/:name', function(req,res)   {
+// app.get('/submit-name/:name', function(req,res)   {
+app.get('/submit-name', function(req,res)   {
+   
     //get the name from the request
-    var name = req.params.name;   //TODO
+    var name = req.query.name;   //TODO
     names.push(name);
     //JSON : javascript object notation
     res.send(JSON.stringify(names));
